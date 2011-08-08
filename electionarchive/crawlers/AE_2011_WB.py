d66c31861ef2f7fac47369640dc2be77a3505f65
Anand Chitipothu <anandology@gmail.com>
2011-08-08 23:20:19 +0530

    AE-2011-WB: download expenditure docs as well.

--- electionarchive/crawlers/AE_2011_WB.py
+++ electionarchive/crawlers/AE_2011_WB.py
@@ -22,22 +22,25 @@ class Crawler(BaseCrawler):
             "election_type": "assembly",
             "year": "2011",
             "url": "http://www.ceowb.in/districtlistaffidavits.aspx",
-            "constituencies": []
+            "constituencies": list(self.get_all_constituencies())
         }
+        return d
         
+    def download_all(self):
+        for c in self.get_all_candidates():
+            logger.info("downloading affidavits of %s (%s)" % (c['name'], c['id']))
+            self.download_affidavits(c['affidavit_id'])
+            
+        self.download_links()
+        self.download_expenditures()
+            
+    def get_all_constituencies(self):
         for dist in self.get_districts():
             for c in self.get_constituencies(dist['id']):
                 c['candidates'] = self.get_candidates(c['id'])
                 c['district'] = dist['name']
                 c['district_id'] = dist['id']
-                d['constituencies'].append(c)
-        return d
-        
-    def download_all(self):
-        self.download_links()
-        for c in self.get_all_candidates():
-            logger.info("downloading affidavits of %s (%s)" % (c['name'], c['id']))
-            self.download_affidavits(c['affidavit_id'])
+                yield c
         
     def get_all_candidates(self):
         d = self.get_data()
@@ -144,7 +147,7 @@ class Crawler(BaseCrawler):
             self._download_affidavit(AffidavitsID, "CR", formdata, "ctl00$ContentPlaceHolder1$dlsCr$ctl00$lnkbtnDownload")
             self._download_affidavit(AffidavitsID, "SC", formdata, "ctl00$ContentPlaceHolder1$dlsSC$ctl00$lnkbtnDownload")
         except Exception:
-            logger.error("failed to download affidavits", exe_info=True)
+            logger.error("failed to download affidavits", exc_info=True)
         
     @disk_memoize("files/%(AffidavitsID)s-%(suffix)s.pdf")
     def _download_affidavit(self, AffidavitsID, suffix, formdata, target):
@@ -171,11 +174,77 @@ class Crawler(BaseCrawler):
         for a in links:
             href = a.get('href', '')
             if href.startswith("javascript:show_window"):
-                print a
                 filename = re.sub(r"javascript:show_window\('./(mis_pdf/.*)'\)", r'\1', href)
                 url = "http://ceowestbengal.nic.in/" + filename
                 title = self.get_text(a)
                 yield title, filename, url
+                
+    def download_expenditures(self):
+        for cons in self.get_expenditure_monitoring():
+            for c in cons:
+                print "c", c
+                self._download_expenditures_for_ac(c['download_id'])
+        
+    def _download_expenditures_for_ac(self, id):
+        url = "http://www.ceowb.in/ViewExpenditureMonitoring.aspx?ID=" + str(id)
+        soup = self.get_soup(url)
+        inputs = soup.findAll("input", {"type": "hidden"})
+        formdata = dict((i['name'], i['value']) for i in inputs)
+        
+        post_links = [a for a in soup.findAll("a") if a['href'].startswith("javascript:__doPostBack")]
+        
+        try:
+            assert post_links[0]['href'] == "javascript:__doPostBack('ctl00$ContentPlaceHolder1$dlsCr$ctl00$lnkbtnDownload','')"
+            assert post_links[1]['href'] == "javascript:__doPostBack('ctl00$ContentPlaceHolder1$dlsSC$ctl00$lnkbtnDownload','')"
+            assert len(post_links) == 2
+        
+            self._download_expenditure(id, "abstract", formdata, "ctl00$ContentPlaceHolder1$dlsCr$ctl00$lnkbtnDownload")
+            self._download_expenditure(id, "affidavit", formdata, "ctl00$ContentPlaceHolder1$dlsSC$ctl00$lnkbtnDownload")
+        except Exception:
+            logger.error("failed to download expenditure docs", exc_info=True)
+            
+    @disk_memoize("files/expediture/%(id)s-%(suffix)s.pdf")
+    def _download_expenditure(self, id, suffix, formdata, target):
+        url = "http://www.ceowb.in/ViewExpenditureMonitoring.aspx?ID=" + str(id)
+        data = dict(formdata)
+        data['__EVENTTARGET'] = target
+        return self.post(url, data)            
+                
+    def get_expenditure_monitoring(self):
+        return [self.get_expenditure_for_ac(c['id']) for c in list(self.get_all_constituencies())[:5]]
+        
+    @disk_memoize("data/expenditure/%(acid)s.json")
+    def get_expenditure_for_ac(self, acid):
+        url = "http://ceowb.in/ExpenditureMonitoringForAc.aspx?ACID=" + str(acid)
+        soup = self.get_soup(url)
+        table = soup.find("table", {"id": "ctl00_ContentPlaceHolder1_GridView1"})
+        def parse(tr):
+            cells = tr.findAll("td")
+            download_url = urllib.basejoin(url, cells[4].find("a")['href'])
+            download_id = download_url.split("=")[1] # split ?ID=2
+            
+            return {
+                "id": cells[0].text.strip(),
+                "name": cells[1].text.strip(),
+                "gender": cells[2].text.strip(),
+                "party": cells[3].text.strip(),
+                "download_url": download_url,
+                "download_id": download_id,
+                "affidavits": [
+                    {
+                        "filename": "files/expenditure/%s-abstract.pdf" % download_id,
+                        "name": "Abstract Statement"
+                    },
+                    {
+                    "filename": "files/expenditure/%s-affidavit.pdf" % download_id,
+                        "name": "Affidavit on Accounts"
+                    }
+                ]
+                
+            }
+    
+        rows = table.findAll("tr")[1:] # skip header
+        return [parse(tr) for tr in rows]
 
 def main():
     FORMAT = "%(asctime)s [%(name)s] [%(levelname)s] %(message)s"
@@ -185,7 +254,6 @@ def main():
     # crawler.get_candidates(1)    
     #print crawler.download_affidavits(76)
     crawler.download_all()
-    #print crawler.get_links()
         
 if __name__ == '__main__':
     main()
